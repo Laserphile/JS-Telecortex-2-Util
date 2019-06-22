ff26e46cfe6a64d5a00ce1abbe72618f438e07a9
Derwent <derwent.m@thoughtworks.com>
2019-06-22 19:03:57 +1000

    🎨 take SERVER_CONF and RPI_SPIDEVS out of util

--- src/util/index.js
+++ src/util/index.js
@@ -62,37 +62,7 @@ export const createGammaTable = (steps, gamma) =>
 
 export const SK9822_GAMMA = createGammaTable(uint8Max, 2.8);
 
-export const RPI_SPIDEVS = {
-  0: {
-    bus: 0,
-    device: 0
-  },
-  1: {
-    bus: 0,
-    device: 1
-  },
-  2: {
-    bus: 1,
-    device: 0
-  },
-  3: {
-    bus: 1,
-    device: 1
-  }
-};
-
 export const opcPort = 42069;
-
-export const SERVER_CONF = {
-  // port used to listen for OPC commands
-  opcPort,
-  // SPI Clock Speed
-  spiClockSpeed: 10e6,
-  // SPI Data Mode
-  spiMode: 0
-};
-
-export const CLIENT_CONF = {};
 
 export const FRESH_CONTEXT = {
   // Frame counter for FPS calculation
