fde509ccc941a34273d5cd795c7250f85d1e7927
Derwent <derwentx@gmail.com>
2019-07-23 15:13:44 +1000

    move toBeCloseToBytes into importable testing file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,24 +1,6 @@
 var expect = require('expect');
+import {toBeCloseToBytes} from './testing.js'
+
 expect.extend({
-  toBeCloseToBytes(received, argument) {
-    // console.log(received);
-    const pass = received.length === argument.length;
-    if (!pass) {
-      return {
-        message: () =>
-          `expected ${JSON.stringify(received)} to be the same length as ${JSON.stringify(
-            argument
-          )}`,
-        pass
-      };
-    }
-    received.forEach((vector, index) => {
-      expect(vector.length).toEqual(argument[index].length);
-    });
-    return {
-      message: () =>
-        `expected ${JSON.stringify(received)} to be close to matrix ${JSON.stringify(argument)}`,
-      pass
-    };
-  }
+  toBeCloseToBytes
 });
